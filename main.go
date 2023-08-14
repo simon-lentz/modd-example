ba4b8a5f843262ddaa1729b8493557bb5141aa71
simon-lentz <simonlentz1@gmail.com>
2023-08-14 14:23:57 -0400

    use chi

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // on file mutation in working dir the modd daemon updates the build
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome!</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Welcome!</h1>")
+	})
 	fmt.Println("Starting server on :3000...")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	if err := http.ListenAndServe(":3000", r); err != nil {
 		fmt.Println(err)
 	}
 }
